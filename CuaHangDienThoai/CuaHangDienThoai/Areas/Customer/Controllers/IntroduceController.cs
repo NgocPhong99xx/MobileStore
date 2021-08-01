af8c77799c927a11473d5597da5dcdef5a638fed
NGOCPHONG <you@example.com>
2021-08-01 09:31:08 +0700

    first commit

---
+++ CuaHangDienThoai/CuaHangDienThoai/Areas/Customer/Controllers/IntroduceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CuaHangDienThoai.Areas.Customer.Identity;
+
+namespace CuaHangDienThoai.Areas.Customer.Controllers
+{
+    public class IntroduceController : Controller
+    {
+        [Area("Customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
